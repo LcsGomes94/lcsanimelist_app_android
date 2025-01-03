364abf29ace85e7b315a2a7b5806cf0d1a4d1403
Lucas Gomes <lucas.gomes@inchurch.com.br>
2025-01-03 03:41:31 -0300

    added signing key for release variant

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,15 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("D:\\Lucas\\lcs_signature.jks")
+            storePassword = "ch0c0l4t3"
+            keyAlias = "lcskey"
+            keyPassword = "ch0c0l4t3"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -26,6 +35,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
